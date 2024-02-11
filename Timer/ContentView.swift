47ceb9bf8d74d2fe73e720f733158fffbf28f12b
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-11 22:55:16 +0900

    Day2 Lesson1 終了

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -8,6 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    // タイマーの変数を作成
+    @State var timerHandler: Timer?
+    // カウント(経過時間)の変数を作成
+    @State var count = 0
+    // 永続化する秒数設定(初期値は10)
+    @AppStorage("timer_value") var timerValue = 10
+    // アラート表示有無
+    @State var showAlert = false
+
     var body: some View {
         NavigationStack{
             // 奥から手前方向にレイアウト
@@ -24,7 +33,7 @@ struct ContentView: View {
                 // view(部品)間の感覚を30にする
                 VStack(spacing: 30.0) {
                     // テキストを表示する
-                    Text("残り10秒")
+                    Text("残り\(timerValue - count)秒")
                     // 文字のサイズを指定
                         .font(.largeTitle)
                     // 水平にレイアウト(横方向にレイアウト)
@@ -32,6 +41,8 @@ struct ContentView: View {
                         // スタートボタン
                         Button {
                             // ボタンをタップしたときのアクション
+                            // タイマーをカウントダウン開始する関数を呼び出す
+                            startTimer()
                         } label: {
                             // テキストを表示する
                             Text("スタート")
@@ -49,6 +60,14 @@ struct ContentView: View {
                     // ストップボタン
                         Button {
                             // ボタンをタップしたときのアクション
+                            // timerHandlerをアンラップ
+                            if let timerHandler {
+                                // もしタイマーが、実行中だったら停止
+                                if timerHandler.isValid == true {
+                                    // タイマー停止
+                                    timerHandler.invalidate()
+                                }
+                            }
                         } label: {
                             // テキストを表示する
                             Text("ストップ")
@@ -65,7 +84,12 @@ struct ContentView: View {
                         } // ストップボタンはここまで
                     } //HStack
                 } //VStack ここまで
-            } //ZStack
+            } //ZStack ここまで
+            // 画面が表示されるときに実行される
+            .onAppear {
+                // カウント(経過時間)の変数を初期化
+                count = 0
+            } // .onAppear ここまで
             // ナビゲーションにボタンを追加
             .toolbar{
                 // ナビゲーションバーの右にボタンを追加
@@ -79,9 +103,57 @@ struct ContentView: View {
                     }// NavigationLink
                 } // toolbarItem
             } // .toolbar
+            // 状態変数showAlertがtrueになったときに実行される
+            .alert("終了", isPresented: $showAlert) {
+                Button("OK") {
+                    // OKをタップしたときにここが実行される
+                    print("OKタップされました")
+                }
+            } message: {
+                Text("タイマー終了時間です")
+            } // .alert ここまで
         } //NavigationStack
+    } // body ここまで
 
-    } // body
+    // 1秒ごとに実行されてカウントダウンする
+    func countDownTimer() {
+        // count(経過時間)に+1指定していく
+        count += 1
+
+        // 残りの時間が0以下の時、タイマーを止める
+        if timerValue - count <= 0 {
+            //タイマー停止
+            timerHandler?.invalidate()
+
+            // アラート表示する
+            showAlert = true
+        }
+    } // countDownTimer ここまで
+
+    // タイマーをカウントダウン開始する関数
+    func startTimer () {
+        // timerHandlerをアンラップ
+        if let timerHandler {
+            // もしタイマーが、実行中ならスタートしない
+            if timerHandler.isValid == true {
+                // 何も処理しない
+                return
+            }
+        }
+
+        //残り時間が0以下のとき、count(経過時間)を0に初期化する
+        if timerValue - count <= 0 {
+            // count(経過時間)を0に初期化する
+            count = 0
+        }
+
+        // タイマーをスタート
+        timerHandler = Timer.scheduledTimer(withTimeInterval:1, repeats: true) { _ in
+            // タイマー実行時に呼び出される
+            // 1秒ごとに実行されてカウントダウンする関数を実行する
+            countDownTimer()
+        }
+    } // startTimer ここまで
 } //ContentView
 
 #Preview {
--- Timer/SettingView.swift
+++ Timer/SettingView.swift
@@ -8,8 +8,8 @@
 import SwiftUI
 
 struct SettingView: View {
-    // 秒数設定
-    @State var timerValue = 10
+    // 永続化する秒数設定(初期値は10)
+    @AppStorage("timer_value") var timerValue = 10
     var body: some View {
 // 奥から手前方向にレイアウト
         ZStack {
