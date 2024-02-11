b242296f808905397dcd03a9b2fea1c20715482a
tomohiro1205 <tomohiro_1205@yahoo.co.jp>
2024-02-11 21:48:58 +0900

    Day2 Lesson1-2 終了

--- Timer/ContentView.swift
+++ Timer/ContentView.swift
@@ -18,8 +18,53 @@ struct ContentView: View {
                     .resizable()
                 // セーフエリアを超えて画面全体に配慮する
                     .ignoresSafeArea()
-                // アスペクト比(縦横比)を維持して短辺基準に収まるようにする
+                //　アスペクト比(縦横比)を維持して短辺基準に収まるようにする
                     .scaledToFill()
+                // 垂直にレイアウト　(縦方向にレイアウト)
+                // view(部品)間の感覚を30にする
+                VStack(spacing: 30.0) {
+                    // テキストを表示する
+                    Text("残り10秒")
+                    // 文字のサイズを指定
+                        .font(.largeTitle)
+                    // 水平にレイアウト(横方向にレイアウト)
+                    HStack {
+                        // スタートボタン
+                        Button {
+                            // ボタンをタップしたときのアクション
+                        } label: {
+                            // テキストを表示する
+                            Text("スタート")
+                            //文字のサイズを指定
+                                .font(.title)
+                            // 文字色を白に指定
+                                .foregroundStyle(Color.white)
+                            // 幅と高さを140に指定
+                                .frame(width: 140, height: 140)
+                            // 背景を設定
+                                .background(Color.start)
+                            // 円形に切り抜く
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        } // スタートボタンはここまで
+                    // ストップボタン
+                        Button {
+                            // ボタンをタップしたときのアクション
+                        } label: {
+                            // テキストを表示する
+                            Text("ストップ")
+                            //文字のサイズを指定
+                                .font(.title)
+                            // 文字色を白に指定
+                                .foregroundStyle(Color.white)
+                            // 幅と高さを140に指定
+                                .frame(width: 140, height: 140)
+                            // 背景を設定
+                                .background(Color.stop)
+                            // 円形に切り抜く
+                                .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+                        } // ストップボタンはここまで
+                    } //HStack
+                } //VStack ここまで
             } //ZStack
             // ナビゲーションにボタンを追加
             .toolbar{
--- Timer/SettingView.swift
+++ Timer/SettingView.swift
@@ -8,8 +8,49 @@
 import SwiftUI
 
 struct SettingView: View {
+    // 秒数設定
+    @State var timerValue = 10
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+// 奥から手前方向にレイアウト
+        ZStack {
+            // 背景色表示
+            Color.backgroundSetting
+            // セーフエリアを超えて画面全体に配置します
+                .ignoresSafeArea()
+
+            // 垂直にレイアウト(縦方向にレイアウト)
+            VStack {
+                // スペースを空ける
+                Spacer()
+                // テキストを表示する
+                Text("\(timerValue)秒")
+                // 文字のサイズを指定
+                    .font(.largeTitle)
+                // スペースを空ける
+                Spacer()
+                // Pickerを表示
+                Picker(selection: $timerValue) {
+                    Text("10")
+                        .tag(10)
+                    Text("20")
+                        .tag(20)
+                    Text("30")
+                        .tag(30)
+                    Text("40")
+                        .tag(40)
+                    Text("50")
+                        .tag(50)
+                    Text("60")
+                        .tag(60)
+                } label: {
+                    Text("選択")
+                }
+                // Pickeerをホイール表示
+                .pickerStyle(.wheel)
+                // スペースを空ける
+                Spacer()
+            } // VStack ここまで
+        } //ZStack ここまで
     }
 }
 
